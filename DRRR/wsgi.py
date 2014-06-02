c7cb138b08774ded3e62bb5c95205ad5c8031b94
Wang Yingfei <clause@sjtu.edu.cn>
2014-06-03 01:05:36 +0800

    add route to path in wsgi

--- DRRR/wsgi.py
+++ DRRR/wsgi.py
@@ -14,6 +14,19 @@ framework.
 
 """
 import os
+import sys
+from os import path
+
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "hanz_partners.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DRRR.settings")
+
+projectPath = path.dirname(path.dirname(path.abspath(__file__)))
+if not projectPath in sys.path:
+	sys.path.append(projectPath)
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DRRR.settings")
 
